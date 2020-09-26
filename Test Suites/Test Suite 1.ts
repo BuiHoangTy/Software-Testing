429248fabd2035f795516c8dfa589684a7810c35
BuiHoangTy <62938277+BuiHoangTy@users.noreply.github.com>
2020-09-26 18:49:32 +0700

    Add files via upload

---
+++ Test Suites/Test Suite 1.ts
@@ -0,0 +1,44 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test Suite 1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>0afe8324-8c3f-47ba-8f43-7f750d34b651</testSuiteGuid>
+   <testCaseLink>
+      <guid>25662914-cc0a-4982-b179-d5c3bc096297</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/test 1</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>f3edc65d-deef-4956-b886-abb7b6887151</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/test 2</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>91473f1c-4775-466b-892f-7113bf60e3f9</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/test 3</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>c3b4b8ac-7ce9-4520-9326-e7e28fa94413</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/test 4</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>44335d9a-2c8a-49c0-8e55-9e07d299f786</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/test 5</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
